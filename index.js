a044ec5e9f3cec1f7f138030f400afaca5bd5b01
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2021-05-03 14:09:38 +0530

    parser middleware added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
 require('./models/Users')
@@ -14,6 +15,9 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,10 +25,13 @@ app.use(
     })
 )
 
+
+
 app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/authRoutes')(app)
+require('./routes/billlingRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 
