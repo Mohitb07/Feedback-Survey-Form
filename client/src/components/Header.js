e7846fc54a7c4392e42df42f2bc2f6e7982d08af
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2021-05-03 14:26:59 +0530

    conditional rendering based on user authentication status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,20 @@
 import React , {Component} from 'react';
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
       switch(this.props.auth){
         case null:
-          return 'Loading'
+          return;
         case false:
-          return 'not logged in'
+          return <li><a href="/auth/google">Login With Google</a></li>
         default:
-          return 'logged in'
-          
+          return [
+            <li key="1"><Payments/></li>,
+            <li key="2"><a href="/api/logout">Logout</a></li>          
+        ]
       }
     }
   
@@ -19,7 +23,7 @@ class Header extends Component {
         return(
             <nav>
             <div className="nav-wrapper">
-              <a href="#" className="brand-logo">Emaily</a>
+              <Link to={this.props.auth ? '/surveys': '/' } className="brand-logo">Emaily</Link>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 {this.renderContent()}
               </ul>
