5f0f2fa9dce4183d6b533746d6c9b5ef4322d984
mohitb07 <bmohit980@gmail.com>
2021-05-07 14:23:23 +0530

    showing credits in a  navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,6 +13,7 @@ class Header extends Component {
         default:
           return [
             <li key="1"><Payments/></li>,
+            <li style={{margin: '0 10px'}} key="1">Credits: {this.props.auth.credits}</li>,
             <li key="2"><a href="/api/logout">Logout</a></li>          
         ]
       }
